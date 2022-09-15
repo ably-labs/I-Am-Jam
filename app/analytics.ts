19292c0576ce61b449676dd057947c710dd85d52
Jo Franchetti <jofranchetti@gmail.com>
2022-09-15 18:11:51 +0100

    Fixed casing

--- app/analytics.ts
+++ app/analytics.ts
@@ -5,7 +5,7 @@ const gaUa = import.meta.env.VITE_GA_UA || "";
 if (gaUa && gaUa !== "") {
 
     const gtagScript = document.createElement("script");
-    gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=" + gaUA;
+    gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=" + gaUa;
     document.head.appendChild(gtagScript);
     
     window["dataLayer"] = window["dataLayer"] || [];
