7c0143cfc77a8e59e42ccaac9b5d2269faba4456
Jo Franchetti <jofranchetti@gmail.com>
2022-10-18 08:06:29 +0100

    Scores exist on overview page.

--- app/spectate.ts
+++ app/spectate.ts
@@ -1,22 +1,25 @@
 import Ably from "ably/promises";
-import { AblySpectatorConnector } from "./game/spectating/AblySpectatorConnector";
+import { AblyHighScoreRepository } from "./game/highscores/AblyHighScoreRepository";
+import { Scoreboard } from "./game/highscores/Scoreboard";
+import { AblySpectatorConnector, StateMessage } from "./game/spectating/AblySpectatorConnector";
 
 console.log("Hello from spectate.ts");
 
 const ably = new Ably.Realtime({ authUrl: "/api/ably-token-request" });
 const connector = new AblySpectatorConnector(ably);
+const scoresRepo = new AblyHighScoreRepository(ably);
 
 const guys = document.getElementById("guys");
 
 const origialWidth = 8233;
 const originalHeight = 552;
-
 const scaleWidth = guys.clientWidth / origialWidth;
 const scaleHeight = guys.clientHeight / originalHeight;
 
-connector.subscribe((state) => {
-    console.log(state);
+connector.subscribe(renderPlayer);
+scoresRepo.subscribe(renderScoreboard);
 
+function renderPlayer(state: StateMessage) {
     const scaledX = state.x * scaleWidth;
     const scaledY = (state.y + 10) * scaleHeight;
     
@@ -43,5 +46,20 @@ connector.subscribe((state) => {
 
     guy.style.left = scaledX + "px";
     guy.style.top = invertedY + "px";
+}
 
-});
+function renderScoreboard(scoreboard: Scoreboard) {
+    const tempate = document.getElementById("score-item") as HTMLTemplateElement;
+    const scoreboardContainer = document.getElementById("scores-list") as HTMLDivElement;
+
+    scoreboardContainer.innerHTML = "";
+
+    for (const score of scoreboard.scores) {
+        const clone = tempate.content.cloneNode(true) as HTMLDivElement;
+        clone.querySelector(".name").innerHTML = score.name;
+        clone.querySelector(".time").innerHTML = score.score.toString();
+        scoreboardContainer.appendChild(clone);
+    }   
+
+    console.log("rendered scores");
+}
