886ef84a728c3c104f7d41bd51366923a20c67b3
Jo Franchetti <jofranchetti@gmail.com>
2022-11-14 10:35:49 +0000

    roll back to working and dedupe

--- app/game/highscores/Scoreboard.ts
+++ app/game/highscores/Scoreboard.ts
@@ -1,21 +1,34 @@
 export class Scoreboard {
 
-    private items: { name: string; score: number; }[];
-    private cap: number;
-  
-    public get scores(): { name: string; score: number; }[] {
-      return this.items;
-    }
-  
-    constructor(items: { name: string; score: number; }[] = null, cap: number = 10) {
-      this.items = [];
-      this.cap = cap;
-      this.addRange(items || []);
-    }
-  
-    public addRange(items: { name: string; score: number; }[]) {
-      this.items.push(...items);
+  private items: { name: string; score: number; }[];
+  private cap: number;
+
+  public get scores(): { name: string; score: number; }[] {
+    return this.items;
+  }
+
+  constructor(items: { name: string; score: number; }[] = null, cap: number = 10) {
+    this.items = [];
+    this.cap = cap;
+    this.addRange(items || []);
+  }
+
+  public addRange(items: { name: string; score: number; }[]) {
+    for (let score of items) {
+
+      if (this.items.filter(x => x.name.trim() === score.name.trim()).length > 0) {
+
+        var currentHighScore = this.items.filter(x => x.name === score.name)[0];
+        this.items = this.items.filter(x => x.name !== score.name);
+
+        var bestScore = Math.min(currentHighScore.score, score.score);
+        score.score = bestScore;
+      }
+
+      this.items.push(score);
+
       this.items.sort((a, b) => a.score - b.score);
       this.items = this.items.slice(0, this.cap);
     }
+  }
 }
